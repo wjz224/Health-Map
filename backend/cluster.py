0366e188711dc21a4c5b60bf8550902af49c212a
Ayon Bhowmick <ayb224@lehigh.edu>
2024-03-02 18:27:35 -0500

    clus

--- backend/cluster.py
+++ backend/cluster.py
@@ -1,6 +1,7 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
 import database
+import copy
 
 class Cluster:
     def __init__(self, eps=0.5, min_samples=5, db = database.Database()):
@@ -9,10 +10,12 @@ class Cluster:
         self.db = db
         self.db_data = self.db.getClusterData()
 
-    def makeDiseaseGroups(self):
+    def makeDiseaseGroups(self, data=None):
         # make groups based on diseases
+        if not data:
+            data = copy.deepcopy(self.db_data)
         diseases = {}
-        for point in self.db_data:
+        for point in data:
             for disease in point['DISEASES']:
                 if disease not in diseases:
                     diseases[disease] = []
@@ -20,3 +23,17 @@ class Cluster:
         for d, i in enumerate(diseases):
             diseases[i] = list(set(diseases[d]))
         return diseases
+
+    def predictDiseaseGroups(self, data=None):
+        # predict disease in people who have an empty disease list based on if they have similar symptoms
+        if not data:
+            data = copy.deepcopy(self.db_data)
+        diseases = self.makeDiseaseGroups(data)
+        for point in data:
+            if len(point['DISEASES']) == 0:
+                for disease in diseases:
+                    if len(point["DISEASES"]) > 0:
+                        break
+                    if len(set(point['SYMPTOMS']).intersection(set(diseases[disease]))) >= 2:
+                        point['DISEASES'].append(disease)
+        return self.makeDiseaseGroups(data)
