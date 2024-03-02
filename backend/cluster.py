1ef9bc6e2fa10452385cb3ae9b02c21e4d504aac
Ayon Bhowmick <ayb224@lehigh.edu>
2024-03-02 18:41:49 -0500

    dbscan

--- backend/cluster.py
+++ backend/cluster.py
@@ -10,7 +10,7 @@ class Cluster:
         self.db = db
         self.db_data = self.db.getClusterData()
 
-    def makeDiseaseGroups(self, data=None):
+    def makeDiseaseGroups(self, data=None) -> dict:
         # make groups based on diseases
         if not data:
             data = copy.deepcopy(self.db_data)
@@ -24,7 +24,7 @@ class Cluster:
             diseases[i] = list(set(diseases[d]))
         return diseases
 
-    def predictDiseaseGroups(self, data=None):
+    def predictDiseaseGroups(self, data=None) -> dict:
         # predict disease in people who have an empty disease list based on if they have similar symptoms
         if not data:
             data = copy.deepcopy(self.db_data)
@@ -37,3 +37,27 @@ class Cluster:
                     if len(set(point['SYMPTOMS']).intersection(set(diseases[disease]))) >= 2:
                         point['DISEASES'].append(disease)
         return self.makeDiseaseGroups(data)
+
+    def cluster(self, data=None) -> dict:
+        # cluster the data based on location, symptoms and diseases but giving a higher weight to diseases
+        if not data:
+            data = copy.deepcopy(self.db_data)
+        all_diseases = self.db.getDiseaseList()
+        all_symptoms = self.db.getSymptomList()
+        X = []
+        for point in data:
+            x = [point['LATITUDE'], point['LONGITUDE']]
+            for disease in all_diseases:
+                x.append(1 if disease in point['DISEASES'] else 0)
+            for symptom in all_symptoms:
+                x.append(1 if symptom in point['SYMPTOMS'] else 0)
+            X.append(x)
+        X = np.array(X)
+        db = DBSCAN(eps=self.eps, min_samples=self.min_samples).fit(X)
+        labels = db.labels_
+        clusters = {}
+        for i, label in enumerate(labels):
+            if label not in clusters:
+                clusters[label] = []
+            clusters[label].append(data[i])
+        return clusters
